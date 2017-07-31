c61d9a3a199cd9f34e65124c0d8a9d997c6c809e
Dmitry Solop <dmtsolop@gmail.com>
2017-07-31 05:11:21 +0300

    first commit

---
+++ DSCalculator/Models/DSCalculator.swift
@@ -0,0 +1,13 @@
+//
+//  DSCalculator.swift
+//  DSCalculator
+//
+//  Created by Mac on 27.07.17.
+//  Copyright © 2017 Dmitry. All rights reserved.
+//
+
+import UIKit
+
+class DSCalculator: NSObject {
+
+}
---
+++ DSCalculator/Views/DSButtons.swift
@@ -0,0 +1,21 @@
+//
+//  DSButtons.swift
+//  DSCalculator
+//
+//  Created by Mac on 27.07.17.
+//  Copyright © 2017 Dmitry. All rights reserved.
+//
+
+import UIKit
+
+class DSButtons: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
